ebcef2ff6e57d5a9bc78bb567d0cba126c0df0b0
kghorbanzadeh23 <kamand.ghorbanzadeh@ucalgary.ca>
2024-03-03 10:44:53 -0700

    ex2 is done

--- ex2.py
+++ ex2.py
@@ -1,104 +1,110 @@
 import random
 import timeit
 
-class ArrayQueue:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.queue = [None] * capacity
-        self.head = -1
-        self.tail = -1
-
-    def enqueue(self, item):
-        if (self.tail + 1) % self.capacity == self.head:
-            print("Queue is full. Unable to enqueue item.")
-            return
-        elif self.is_empty():
-            self.head = 0
-            self.tail = 0
-        else:
-            self.tail = (self.tail + 1) % self.capacity
-        self.queue[self.tail] = item
-        self.merge_sort(self.queue)
-    
-    def merge_sort(arr):
-        if len(arr) > 1:
-            mid = len(arr)//2
-            L = arr[:mid]
-            R = arr[mid:]
-
-            self.merge_sort(L)
-            self.merge_sort(R)
-
-            i = j = k = 0
-
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
-
-        # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
-
-
-    def dequeue(self):
-        if self.is_empty():
-            print("Queue is empty. Unable to dequeue item.")
-            return None
-        item = self.queue[self.head]
-        if self.head == self.tail:
-            self.head = -1
-            self.tail = -1
-        else:
-            self.head = (self.head + 1) % self.capacity
-        return item
-    
-class PriorityQueue:
+class PriorityQueueMergeSort:
     def __init__(self):
         self.queue = []
-
-    def enqueue(self, item):
-        if len(self.queue) == 0:
-            self.queue.append(item)
-        else:
-            index = 0
-            while index < len(self.queue) and self.queue[index] < item:
-                index += 1
-            self.queue.insert(index, item)
-
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+        self.queue = self.merge_sort(self.queue)
+    
     def dequeue(self):
-        if self.is_empty():
-            print("Queue is empty. Unable to dequeue item.")
+        if len(self.queue) == 0:
             return None
         return self.queue.pop(0)
     
-    def is_empty(self):
-        return self.head == -1
-    
-def generate_random_tasks():
-    tasks = []
-    for i in range(1000):
-        if random.random() < 0.7:
-            tasks.append("enqueue")
-        else:
-            tasks.append("dequeue")
-        return tasks
-    
-num_lists = 100
-sort_time = timeit.timeit(stmt="pq.enqueue('task')", setup="from __main__ import ArrayQueue; pq = ArrayQueue(1000); import random; random.seed(42); tasks = generate_random_tasks()", globals=globals(), number=num_lists)
-insert_time = timeit.timeit(stmt="pq.enqueue('task')", setup="from __main__ import PriorityQueue; pq = PriorityQueue(); import random; random.seed(42); tasks = generate_random_tasks()", globals=globals(), number=num_lists)
+    def merge_sort(self, array):
+        if len(array) > 1:
+            mid = len(array) // 2
+            left_half = array[:mid]
+            right_half = array[mid:]
 
-print("Time taken for ArrayQueue with sorting:", sort_time)
-print("Time taken for PriorityQueue with insertion:", insert_time)
+            self.merge_sort(left_half)
+            self.merge_sort(right_half)
+
+            i = j = k = 0
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] < right_half[j]:
+                    array[k] = left_half[i]
+                    i += 1
+                else:
+                    array[k] = right_half[j]
+                    j += 1
+                k += 1
+
+            while i < len(left_half):
+                array[k] = left_half[i]
+                i += 1
+                k += 1
+
+            while j < len(right_half):
+                array[k] = right_half[j]
+                j += 1
+                k += 1
+        return array
+
+class PriorityQueueSortedInsert:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, value):
+        if not self.queue:
+            self.queue.append(value)
+        else:
+            for i in range(len(self.queue)):
+                if value < self.queue[i]:
+                    self.queue.insert(i, value)
+                    break
+            else:
+                self.queue.append(value)
+    
+    def dequeue(self):
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)
+
+def generate_tasks():
+    tasks = []
+    for _ in range(1000):
+        if random.random() < 0.7:
+            tasks.append(('enqueue', random.randint(0, 100)))
+        else:
+            tasks.append(('dequeue', None))
+    return tasks
+
+def execute_tasks(tasks, pq_class):
+    pq = pq_class()
+    for operation, value in tasks:
+        if operation == 'enqueue':
+            pq.enqueue(value)
+        elif operation == 'dequeue':
+            pq.dequeue()
+
+def measure_performance(task_lists, pq_class):
+    times = []
+    for tasks in task_lists:
+        start_time = timeit.default_timer()
+        execute_tasks(tasks, pq_class)
+        end_time = timeit.default_timer()
+        times.append(end_time - start_time)
+    return sum(times) / len(times)
+
+# Generating 100 random lists of tasks
+task_lists = [generate_tasks() for _ in range(100)]
+
+# Measuring the performance
+average_time_merge_sort = measure_performance(task_lists, PriorityQueueMergeSort)
+average_time_sorted_insert = measure_performance(task_lists, PriorityQueueSortedInsert)
+
+print(f"Average time for MergeSort-based PQ: {average_time_merge_sort}")
+print(f"Average time for SortedInsert-based PQ: {average_time_sorted_insert}")
+
+"""
+5) SortedInsert is faster than MergeSort because of the time complexities between the two sorts.
+MergeSort has a time complexity of O(nlogn) for sorting an array of n elements. When applied
+to each enqueue operation, this complexity can add up, especially as the size of the queue grows.
+The time complexity for inserting an element into a sorted array is O(n) in the worst case
+(if every insertion happens at the very end). However, the average case can be much more
+efficient if elements are often inserted nearer to their correct position.
+"""
